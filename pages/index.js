26a02635591a228f03d06c1445926350fd83642b
aaronleekers <leekaaron5@gmail.com>
2022-12-26 21:50:28 -0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -3,45 +3,78 @@ import { useState } from "react";
 import styles from "./index.module.css";
 
 export default function Home() {
-  const [animalInput, setAnimalInput] = useState("");
-  const [result, setResult] = useState();
+  const [input, setInput] = useState("");
+  const [messages, setMessages] = useState([]);
+  const classes = useStyles();
 
   async function onSubmit(event) {
     event.preventDefault();
+
+    // Store the input message in the messages array
+    const newMessage = {
+      text: input,
+    };
+    setMessages([...messages, newMessage]);
+
+    // Send the input message to the API
     const response = await fetch("/api/generate", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify({ animal: animalInput }),
+      body: JSON.stringify({
+        // Use the input message and the existing messages as context for GPT
+        input: input,
+        context: messages.map((m) => m.text).join("\n"),
+      }),
     });
     const data = await response.json();
-    setResult(data.result);
-    setAnimalInput("");
+
+    // Store the response from the API in the messages array
+    const newResponse = {
+      text: data.result,
+    };
+    setMessages([...messages, newResponse]);
+
+    setInput("");
   }
 
   return (
-    <div>
-      <Head>
-        <title>OpenI Quickstart</title>
-        <link rel="icon" href="/dog.png" />
-      </Head>
+    <div className={classes.root}>
+      <div>
+        <title>OpenAI Quickstart</title>
+      </div>
 
-      <main className={styles.main}>
-        <img src="/dog.png" className={styles.icon} />
-        <h3>Name my pet</h3>
-        <form onSubmit={onSubmit}>
-          <input
-            type="text"
-            name="animal"
-            placeholder="Enter an animal"
-            value={animalInput}
-            onChange={(e) => setAnimalInput(e.target.value)}
-          />
-          <input type="submit" value="Generate names" />
-        </form>
-        <div className={styles.result}>{result}</div>
-      </main>
+      <Card className={classes.root}>
+        <CardContent>
+          <Typography variant="h5" className={classes.heading}>
+            Ask a question
+          </Typography>
+          <form onSubmit={onSubmit}>
+            <TextField
+              type="text"
+              name="input"
+              placeholder="Enter a question"
+              value={input}
+              onChange={(e) => setInput(e.target.value)}
+              fullWidth
+              variant="outlined"
+            />
+            <Button type="submit" variant="contained" color="primary">
+              Ask
+            </Button>
+          </form>
+          <div className={classes.chatLog}>
+            {messages.map((message, index) => (
+              <div key={index}>
+                <Typography variant="subtitle1" color="textSecondary">
+                  {message.text}
+                </Typography>
+              </div>
+            ))}
+          </div>
+        </CardContent>
+      </Card>
     </div>
   );
 }
