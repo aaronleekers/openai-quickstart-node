ec48a69c0bda14a9ee2d966aff8bf06651471f63
aaronleekers <leekaaron5@gmail.com>
2022-12-26 21:42:36 -0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -23,7 +23,7 @@ export default function Home() {
   return (
     <div>
       <Head>
-        <title>OpenAI Quickstart</title>
+        <title>OpenI Quickstart</title>
         <link rel="icon" href="/dog.png" />
       </Head>
 
